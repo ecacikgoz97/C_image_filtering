47a2d381e8480e0b2d037febdfb26b956c99fd72
Maco1907 <69140503+ecacikgoz97@users.noreply.github.com>
2021-01-28 11:27:30 +0300

    Update image_filter.c

--- image_filter.c
+++ image_filter.c
@@ -1,6 +1,6 @@
 /*
- * ELEC230 - PE7
- *
+ * This Code is for changing the pixel values of a given
+ * image for filtering.
  */
 
 #include <stdio.h>
@@ -12,8 +12,7 @@
 #define MAX_GRAYVAL 255
 #define BUFFER_SIZE 50
 void readFileToMat(char filename[],int *a, int numRows, int numCols){
-    // Write a C function to read file content into a double array,
-    /* YOUR CODE GOES HERE */
+    // Function to read file content into a double array
     int data;
     FILE *fp = fopen(filename, "r");
     if(fp!=NULL){
@@ -27,8 +26,7 @@ void readFileToMat(char filename[],int *a, int numRows, int numCols){
     }
 }
 void printMatToFile(char filename[],int *m, int numRows, int numCols){
-    // Write a C function to print a double array to an output file
-    /* YOUR CODE GOES HERE */
+    // Function to print a double array to an output file
     int data;
     FILE *fp = fopen(filename, "w");
     for(int i=0; i < numRows; i++){
@@ -41,8 +39,7 @@ void printMatToFile(char filename[],int *m, int numRows, int numCols){
 }
 
 void imNegate(int *n,int *m,int numRows, int numCols){
-    // Write a C function to return the image negative of the original image
-    /* YOUR CODE GOES HERE */
+    // Function to return the image negative of the original image
      for(int i=0; i < numRows; i++){
         for(int j=0; j < numCols; j++){
                 *(n+i*numCols+j) = 255 - *(m+i*numCols+j);
@@ -51,8 +48,7 @@ void imNegate(int *n,int *m,int numRows, int numCols){
 }
 
 void imChange(int *n,int *m,int numRows, int numCols, int changeVal){
-    // Write a C function that updates pixel values as follows: output pixel = input pixel + changeVal
-    /* YOUR CODE GOES HERE */
+    // Function that updates pixel values as follows: output pixel = input pixel + changeVal
     for(int i=0; i < numRows; i++){
         for(int j=0; j < numCols; j++){
 	    if (changeVal + *(m+i*numCols+j) > 255)
@@ -69,8 +65,7 @@ void imChange(int *n,int *m,int numRows, int numCols, int changeVal){
 
 int main(int argc, char* argv[]) {
     
-    // Get your inputs that are passed using command line arguments.
-     /* YOUR CODE GOES HERE */
+    // Get our inputs that are passed using command line arguments.
     char *filename;
     int numRows,numCols, change;
     filename = argv[1];
@@ -85,14 +80,12 @@ int main(int argc, char* argv[]) {
     int orgMat[numRows][numCols];
     readFileToMat(filename,&orgMat[0][0],numRows,numCols);
     
-    // Get an image negative of your input image and write an output file name as \textit{input\_file\_name\_neg.txt}.
-    /* YOUR CODE GOES HERE */
+    // Get output image negative of our input image and write an output file name as \textit{input\_file\_name\_neg.txt}.
     char out_filename[] = "input_file_name_neg.txt"; 
     int negMat[numRows][numCols];
     imNegate(&negMat[0][0], &orgMat[0][0], numRows, numCols);
     printMatToFile(out_filename, &negMat[0][0], numRows, numCols);
-    // Update your input image with the given changeVal, and write an output file name as \textit{input\_file\_name\_changeVal.txt}
-    /* YOUR CODE GOES HERE */
+    // Update our input image with the given changeVal, and write an output file name as \textit{input\_file\_name\_changeVal.txt}
     char out_filename2[] = "input_file_name_changeVal.txt";
     int chgMat[numRows][numCols];
     imChange(&chgMat[0][0], &orgMat[0][0],numRows, numCols, change);
